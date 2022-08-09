4481187a4d2f16b94d7dfa9d9563b38c33eb9cc9
EunhaKyeong <kyeongeh424@gmail.com>
2022-08-09 16:04:36 +0900

    Design: FeedFragment 에서 타입 선택하는 화면 UI 수정 완료

--- app/src/main/java/com/mangpo/taste/view/FeedFragment.kt
+++ app/src/main/java/com/mangpo/taste/view/FeedFragment.kt
@@ -1,8 +1,7 @@
 package com.mangpo.taste.view
 
+import android.graphics.drawable.Drawable
 import android.view.View
-import android.view.ViewTreeObserver
-import android.widget.TextView
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.Observer
@@ -10,6 +9,7 @@ import androidx.navigation.findNavController
 import com.mangpo.taste.R
 import com.mangpo.taste.base.BaseFragment
 import com.mangpo.taste.databinding.FragmentFeedBinding
+import com.mangpo.taste.util.setSpannableText
 import com.mangpo.taste.viewmodel.MainViewModel
 
 class FeedFragment : BaseFragment<FragmentFeedBinding>(FragmentFeedBinding::inflate) {
@@ -18,20 +18,10 @@ class FeedFragment : BaseFragment<FragmentFeedBinding>(FragmentFeedBinding::infl
 
     override fun initAfterBinding() {
         setStartDestination()   //기록이 있으면 timelineFragment, 없으면 noTasteFragment 로 startDestination 지정하기
-
-        //나의 취향 정렬 타입 선택하는 화면에서 paddingLift 설정하기
-        binding.feedMyTasteTv.viewTreeObserver.addOnGlobalLayoutListener(object: ViewTreeObserver.OnGlobalLayoutListener {
-            override fun onGlobalLayout() {
-                binding.feedType1Tv.setPadding(binding.feedMyTasteTv.width, 0, 0, 0)
-                binding.feedType2Tv.setPadding(binding.feedMyTasteTv.width, 0, 0, 0)
-                binding.feedType3Tv.setPadding(binding.feedMyTasteTv.width, 0, 0, 0)
-
-                binding.feedMyTasteTv.viewTreeObserver.removeOnGlobalLayoutListener(this)
-            }
-        })
-
         setMyEventListener()
         observe()
+
+        setSpannableText(binding.feedMyTasteTv.text.toString(), requireContext(), R.color.GY_03, 6, binding.feedMyTasteTv.text.length, binding.feedMyTasteTv)   //나의 취향 뒷부분 텍스트 색상 변경
     }
 
     //기록이 있을 때/없을 때 각각 다른 StartDestination 을 가짐
@@ -61,9 +51,9 @@ class FeedFragment : BaseFragment<FragmentFeedBinding>(FragmentFeedBinding::infl
         }
 
         //취향 정렬 타입 텍스트뷰 클릭 리스너
-        binding.feedType1Tv.setOnClickListener(TouchViewListener())
-        binding.feedType2Tv.setOnClickListener(TouchViewListener())
-        binding.feedType3Tv.setOnClickListener(TouchViewListener())
+        binding.feedType1TouchCl.setOnClickListener(TouchViewListener(1))
+        binding.feedType2TouchCl.setOnClickListener(TouchViewListener(2))
+        binding.feedType3TouchCl.setOnClickListener(TouchViewListener(3))
     }
 
     //나의 취향 정렬 타입 선택하는 화면 숨겨주기
@@ -88,21 +78,46 @@ class FeedFragment : BaseFragment<FragmentFeedBinding>(FragmentFeedBinding::infl
     }
 
     //터치뷰 클릭리스너 이너 클래스
-    inner class TouchViewListener: View.OnClickListener {
-        private val typeList = listOf(getString(R.string.title_timeline), getString(R.string.title_by_sense), getString(R.string.title_by_score), getString(R.string.title_by_calendar))
+    inner class TouchViewListener(private val order: Int): View.OnClickListener {
+        private val typeTextList = listOf(getString(R.string.title_timeline), getString(R.string.title_by_sense), getString(R.string.title_by_score), getString(R.string.title_by_calendar))
+        private val typeIconList = listOf<Drawable>(ContextCompat.getDrawable(requireContext(), R.drawable.ic_timeline_24)!!, ContextCompat.getDrawable(requireContext(), R.drawable.ic_sense_24)!!, ContextCompat.getDrawable(requireContext(), R.drawable.ic_star_24)!!, ContextCompat.getDrawable(requireContext(), R.drawable.ic_calendar_24)!!)
+
+        private lateinit var clickedText: String
+        private lateinit var clickedIcon: Drawable
 
         override fun onClick(v: View?) {
-            mainViewModel.setFeedType((v as TextView).text.toString())  //피드 타입 LiveData 변경
+            when (order) {
+                1 -> {
+                    clickedText = binding.feedType1Tv.text.toString()
+                    clickedIcon = binding.feedType1Iv.drawable
+                }
+                2 -> {
+                    clickedText = binding.feedType2Tv.text.toString()
+                    clickedIcon = binding.feedType2Iv.drawable
+                }
+                else -> {
+                    clickedText = binding.feedType3Tv.text.toString()
+                    clickedIcon = binding.feedType3Iv.drawable
+                }
+            }
 
-            binding.feedTypeTv.text = v.text.toString() //선택한 텍스트를 feedType 텍스트뷰에 보여주기
+
+            mainViewModel.setFeedType(clickedText)  //피드 타입 LiveData 변경
+            binding.feedMyTasteTv.text = "나의 취향 $clickedText" //피드 타입 텍스트 변경
+            setSpannableText(binding.feedMyTasteTv.text.toString(), requireContext(), R.color.GY_03, 6, binding.feedMyTasteTv.text.length, binding.feedMyTasteTv)   //나의 취향 뒷부분 텍스트 색상 변경
 
             hideTypeSelectLayout()  //나의 취향 정렬 타입 선택하는 화면 숨겨주기
 
-            //선택한 타입 텍스트 제외하고 나머지를 typeSelectLayout 에 보여주기
-            val removedTypeList: List<String> = typeList.filterNot { it == v.text.toString() }
-            binding.feedType1Tv.text = removedTypeList[0]
-            binding.feedType2Tv.text = removedTypeList[1]
-            binding.feedType3Tv.text = removedTypeList[2]
+            //선택한 타입 텍스트&아이콘 제외하고 나머지를 typeSelectLayout 에 보여주기
+            val removedTypeTextList: List<String> = typeTextList.filterNot { it == clickedText }
+            val removedTypeIconList: List<Drawable> = typeIconList.filterNot { it == clickedIcon }
+
+            binding.feedType1Tv.text = removedTypeTextList[0]
+            binding.feedType1Iv.setImageDrawable(removedTypeIconList[0])
+            binding.feedType2Tv.text = removedTypeTextList[1]
+            binding.feedType2Iv.setImageDrawable(removedTypeIconList[1])
+            binding.feedType3Tv.text = removedTypeTextList[2]
+            binding.feedType3Iv.setImageDrawable(removedTypeIconList[2])
         }
     }
 }
