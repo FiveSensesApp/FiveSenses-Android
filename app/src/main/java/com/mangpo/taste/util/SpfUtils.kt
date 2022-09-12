be843c9de9c3f63d280624117beeadd7bb39da74
EunhaKyeong <kyeongeh424@gmail.com>
2022-09-13 01:00:46 +0900

    Feat: SharedPreferences, EncryptedSharedPreferences 관련 공통 함수 추가

--- app/src/main/java/com/mangpo/taste/util/SpfUtils.kt
+++ app/src/main/java/com/mangpo/taste/util/SpfUtils.kt
@@ -4,6 +4,20 @@ import com.mangpo.taste.MyApplication.Companion.encryptedSpf
 import com.mangpo.taste.MyApplication.Companion.spf
 
 object SpfUtils {
+    fun writeSpf(key: String, value: String) {
+        with(spf.edit()) {
+            putString(key, value)
+            apply()
+        }
+    }
+
+    fun writeSpf(key: String, value: Int) {
+        with(spf.edit()) {
+            putInt(key, value)
+            apply()
+        }
+    }
+
     fun writeSpf(key: String, value: Boolean) {
         with(spf.edit()) {
             putBoolean(key, value)
@@ -11,7 +25,11 @@ object SpfUtils {
         }
     }
 
-    fun getSpf(key: String, defaultValue: Boolean): Boolean? {
+    fun getStrSpf(key: String): String? {
+        return spf.getString(key, null)
+    }
+
+    fun getBooleanSpf(key: String, defaultValue: Boolean): Boolean? {
         return spf.getBoolean(key, defaultValue)
     }
 
@@ -22,8 +40,19 @@ object SpfUtils {
         }
     }
 
-    fun getEncryptedSpf(key: String): String? {
+    fun getStrEncryptedSpf(key: String): String? {
         return encryptedSpf.getString(key, null)
+    }
+
+    fun writeEncryptedSpf(key: String, value: Int) {
+        with(encryptedSpf.edit()) {
+            putInt(key, value)
+            apply()
+        }
+    }
+
+    fun getIntEncryptedSpf(key: String): Int {
+        return encryptedSpf.getInt(key, -1)
     }
 
     fun clearEncryptedSpf() {
