5a2ee24d064c322f6920797fed26f90f31206dd1
EunhaKyeong <kyeongeh424@gmail.com>
2022-09-13 00:51:09 +0900

    Rename: fragment_no_taste -> fragment_no_feed

--- app/src/main/java/com/mangpo/taste/view/NoFeedFragment.kt
+++ app/src/main/java/com/mangpo/taste/view/NoFeedFragment.kt
@@ -12,14 +12,15 @@ import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.Observer
 import com.mangpo.taste.R
 import com.mangpo.taste.base.BaseFragment
-import com.mangpo.taste.databinding.FragmentNoTasteBinding
+import com.mangpo.taste.databinding.FragmentNoFeedBinding
+import com.mangpo.taste.util.SpfUtils.getStrSpf
 import com.mangpo.taste.util.convertDpToPx
 import com.mangpo.taste.view.calendar.CalendarHeaderViewContainer
 import com.mangpo.taste.view.calendar.DayViewContainer
 import com.mangpo.taste.viewmodel.MainViewModel
 import java.time.LocalDate
 
-class NoTasteFragment : BaseFragment<FragmentNoTasteBinding>(FragmentNoTasteBinding::inflate) {
+class NoFeedFragment : BaseFragment<FragmentNoFeedBinding>(FragmentNoFeedBinding::inflate) {
     private val mainViewModel: MainViewModel by activityViewModels()
 
     private lateinit var sp: SharedPreferences
@@ -34,38 +35,40 @@ class NoTasteFragment : BaseFragment<FragmentNoTasteBinding>(FragmentNoTasteBind
         setMyEventListener()
         observe()
 
-        //tryTv 텍스트 특정 부분 색상 GY_04로 변경
-        val nickname = "워니버니"
-        val ssb: SpannableStringBuilder = SpannableStringBuilder("${nickname}님,\n처음으로 취향을 감각해보세요!")
-        ssb.setSpan(ForegroundColorSpan(ContextCompat.getColor(requireContext(), R.color.GY_04)), 0, nickname.length, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
-        binding.noTasteTryTv.text = ssb
+        val nickname: String? = getStrSpf("nickname")
+        if (nickname!=null) {
+            //tryTv 텍스트 특정 부분 색상 GY_04로 변경
+            val ssb: SpannableStringBuilder = SpannableStringBuilder("${nickname}님,\n처음으로 취향을 감각해보세요!")
+            ssb.setSpan(ForegroundColorSpan(ContextCompat.getColor(requireContext(), R.color.GY_04)), 0, nickname.length, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+            binding.noFeedTryTv.text = ssb
+        }
 
         //가이드 버튼 visibility 여부 확인
         if (sp.getBoolean("isGuideClicked", false))
-            binding.noTasteHowToUseBtn.visibility = View.INVISIBLE
+            binding.noFeedHowToUseBtn.visibility = View.INVISIBLE
         else
-            binding.noTasteHowToUseBtn.visibility = View.VISIBLE
+            binding.noFeedHowToUseBtn.visibility = View.VISIBLE
     }
 
     private fun initDayViewContainer() {
-        dayViewContainer = DayViewContainer(binding.noTasteCv, listOf())
+        dayViewContainer = DayViewContainer(binding.noFeedCv, listOf())
         dayViewContainer.setOnDayClickListener(object : DayViewContainer.OnDayClickListener {
             override fun onClick(oldDate: LocalDate, selectedDate: LocalDate) {
-                binding.noTasteCv.notifyDateChanged(selectedDate)   //현재 선택한 날짜 선택된 UI 로 변경
-                oldDate?.let { binding.noTasteCv.notifyDateChanged(oldDate) }   //이전에 선택한 날짜 선택하지 않은 UI 로 변경
+                binding.noFeedCv.notifyDateChanged(selectedDate)   //현재 선택한 날짜 선택된 UI 로 변경
+                oldDate?.let { binding.noFeedCv.notifyDateChanged(oldDate) }   //이전에 선택한 날짜 선택하지 않은 UI 로 변경
             }
         })
-        binding.noTasteCv.dayBinder = dayViewContainer
+        binding.noFeedCv.dayBinder = dayViewContainer
     }
 
     private fun initCalendarHeader() {
-        headerViewContainer = CalendarHeaderViewContainer(binding.noTasteCv)
-        binding.noTasteCv.monthHeaderBinder = headerViewContainer
+        headerViewContainer = CalendarHeaderViewContainer(binding.noFeedCv)
+        binding.noFeedCv.monthHeaderBinder = headerViewContainer
     }
 
     private fun setMyEventListener() {
         //가이드 버튼 클릭 리스너
-        binding.noTasteHowToUseBtn.setOnClickListener {
+        binding.noFeedHowToUseBtn.setOnClickListener {
             //가이드 노션 페이지로 이동(나중에)
             it.visibility = View.INVISIBLE  //visibility 를 INVISIBLE
 
@@ -76,8 +79,8 @@ class NoTasteFragment : BaseFragment<FragmentNoTasteBinding>(FragmentNoTasteBind
         }
 
         //달력 접기/펼치기 터치뷰 클릭 리스너
-        binding.noTasteArrowTouchView.setOnClickListener {
-            if (binding.noTasteArrowIv.tag=="fold") {  //펼쳐져 있을 때 -> 접기
+        binding.noFeedArrowTouchView.setOnClickListener {
+            if (binding.noFeedArrowIv.tag=="fold") {  //펼쳐져 있을 때 -> 접기
                 setFoldCalendarUI()
                 setTryClMargin(convertDpToPx(requireContext(), 136))    //취향을 감각해보세요! 레이아웃 topMarin 136dp
             } else {    //접혀 있을 때 -> 펼치기
@@ -89,23 +92,23 @@ class NoTasteFragment : BaseFragment<FragmentNoTasteBinding>(FragmentNoTasteBind
 
     //캘린더 레이아웃 높이 설정 함수
     private fun setCalendarClHeight(height: Int) {
-        val params = binding.noTasteCalendarCl.layoutParams
+        val params = binding.noFeedCalendarCl.layoutParams
         params.height = height
-        binding.noTasteCalendarCl.layoutParams = params
+        binding.noFeedCalendarCl.layoutParams = params
     }
 
     //취향을 감각해보세요! 레이아웃 마진 설정 함수
     private fun setTryClMargin(margin: Int) {
-        val params = binding.noTasteTryCl.layoutParams as ConstraintLayout.LayoutParams
+        val params = binding.noFeedTryCl.layoutParams as ConstraintLayout.LayoutParams
         params.setMargins(convertDpToPx(requireContext(), 20), margin, convertDpToPx(requireContext(), 20), 0)
-        binding.noTasteTryCl.layoutParams = params
+        binding.noFeedTryCl.layoutParams = params
     }
 
     //접혀있는 캘린더뷰 UI 화면 함수
     private fun setFoldCalendarUI() {
         setCalendarClHeight(convertDpToPx(requireContext(), 187))   //달력 레이아웃 높이 187dp 로
-        binding.noTasteArrowIv.setImageResource(R.drawable.ic_expand_46)    //펼치기 아이콘으로 변경
-        binding.noTasteArrowIv.tag = "expand"   //이미지뷰 태그를 expand 로
+        binding.noFeedArrowIv.setImageResource(R.drawable.ic_expand_46)    //펼치기 아이콘으로 변경
+        binding.noFeedArrowIv.tag = "expand"   //이미지뷰 태그를 expand 로
 
         dayViewContainer.updateMonthConfiguration(1, null)    //접혀있을 땐 한줄만 보여주기
     }
@@ -113,8 +116,8 @@ class NoTasteFragment : BaseFragment<FragmentNoTasteBinding>(FragmentNoTasteBind
     //펼쳐져있는 캘린더뷰 UI 화면 함수
     private fun setExpandCalendarUI() {
         setCalendarClHeight(convertDpToPx(requireContext(), 470))    //달력 레이아웃 높이 470dp 로
-        binding.noTasteArrowIv.setImageResource(R.drawable.ic_fold_44)  //접기 아이콘으로 변경
-        binding.noTasteArrowIv.tag = "fold" //이미지뷰 태그를 fold 로
+        binding.noFeedArrowIv.setImageResource(R.drawable.ic_fold_44)  //접기 아이콘으로 변경
+        binding.noFeedArrowIv.tag = "fold" //이미지뷰 태그를 fold 로
 
         dayViewContainer.updateMonthConfiguration(6, headerViewContainer.getYearMonth())    //펼쳐져 있을 땐 6줄 보여주기
     }
@@ -126,26 +129,26 @@ class NoTasteFragment : BaseFragment<FragmentNoTasteBinding>(FragmentNoTasteBind
                 getString(R.string.title_timeline) -> {
                     setTryClMargin(convertDpToPx(requireContext(), 9))  //취향을 감각해보세요! 레이아웃 topMarin 9dp
 
-                    binding.noTasteCalendarCl.visibility = View.GONE    //캘린더 레이아웃 GONE
-                    binding.noTasteTimelineFilterRg.visibility = View.VISIBLE
-                    binding.noTasteSenseHsv.visibility = View.INVISIBLE
-                    binding.noTasteScoreFilterRg.visibility = View.INVISIBLE
+                    binding.noFeedCalendarCl.visibility = View.GONE    //캘린더 레이아웃 GONE
+                    binding.noFeedTimelineFilterRg.visibility = View.VISIBLE
+                    binding.noFeedSenseHsv.visibility = View.INVISIBLE
+                    binding.noFeedScoreFilterRg.visibility = View.INVISIBLE
                 }
                 getString(R.string.title_by_sense) -> {
                     setTryClMargin(convertDpToPx(requireContext(), 9))  //취향을 감각해보세요! 레이아웃 topMarin 9dp
 
-                    binding.noTasteCalendarCl.visibility = View.GONE    //캘린더 레이아웃 GONE
-                    binding.noTasteTimelineFilterRg.visibility = View.INVISIBLE
-                    binding.noTasteSenseHsv.visibility = View.VISIBLE
-                    binding.noTasteScoreFilterRg.visibility = View.INVISIBLE
+                    binding.noFeedCalendarCl.visibility = View.GONE    //캘린더 레이아웃 GONE
+                    binding.noFeedTimelineFilterRg.visibility = View.INVISIBLE
+                    binding.noFeedSenseHsv.visibility = View.VISIBLE
+                    binding.noFeedScoreFilterRg.visibility = View.INVISIBLE
                 }
                 getString(R.string.title_by_score) -> {
                     setTryClMargin(convertDpToPx(requireContext(), 9))  //취향을 감각해보세요! 레이아웃 topMarin 9dp
 
-                    binding.noTasteCalendarCl.visibility = View.GONE    //캘린더 레이아웃 GONE
-                    binding.noTasteTimelineFilterRg.visibility = View.INVISIBLE
-                    binding.noTasteSenseHsv.visibility = View.INVISIBLE
-                    binding.noTasteScoreFilterRg.visibility = View.VISIBLE
+                    binding.noFeedCalendarCl.visibility = View.GONE    //캘린더 레이아웃 GONE
+                    binding.noFeedTimelineFilterRg.visibility = View.INVISIBLE
+                    binding.noFeedSenseHsv.visibility = View.INVISIBLE
+                    binding.noFeedScoreFilterRg.visibility = View.VISIBLE
                 }
                 getString(R.string.title_by_calendar) -> {
                     setTryClMargin(convertDpToPx(requireContext(), 136))    //취향을 감각해보세요! 레이아웃 topMarin 136dp
@@ -154,10 +157,10 @@ class NoTasteFragment : BaseFragment<FragmentNoTasteBinding>(FragmentNoTasteBind
                     dayViewContainer.setSelectedDate(LocalDate.now())   //현재 날짜로 변경
                     dayViewContainer.updateMonthConfiguration(1, null)
 
-                    binding.noTasteCalendarCl.visibility = View.VISIBLE //캘린더 레이아웃 VISIBLE
-                    binding.noTasteTimelineFilterRg.visibility = View.INVISIBLE
-                    binding.noTasteSenseHsv.visibility = View.INVISIBLE
-                    binding.noTasteScoreFilterRg.visibility = View.INVISIBLE
+                    binding.noFeedCalendarCl.visibility = View.VISIBLE //캘린더 레이아웃 VISIBLE
+                    binding.noFeedTimelineFilterRg.visibility = View.INVISIBLE
+                    binding.noFeedSenseHsv.visibility = View.INVISIBLE
+                    binding.noFeedScoreFilterRg.visibility = View.INVISIBLE
                 }
             }
         })
