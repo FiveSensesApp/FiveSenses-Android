963791790c234536790cef7ad12bf99ed5f1a142
EunhaKyeong <kyeongeh424@gmail.com>
2022-10-10 03:04:40 +0900

    Design: 닉네임 수정 UI 구현 완료

---
+++ app/src/main/java/com/mangpo/taste/view/UpdateNicknameActivity.kt
@@ -0,0 +1,10 @@
+package com.mangpo.taste.view
+
+import com.mangpo.taste.base.BaseActivity
+import com.mangpo.taste.databinding.ActivityUpdateNicknameBinding
+
+class UpdateNicknameActivity : BaseActivity<ActivityUpdateNicknameBinding>(ActivityUpdateNicknameBinding::inflate) {
+    override fun initAfterBinding() {
+
+    }
+}
