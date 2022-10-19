d480fc94493c7f3cd583fefd18876f183c2391d9
EunhaKyeong <kyeongeh424@gmail.com>
2022-10-19 23:40:17 +0900

    Feat: 성향분석 화면 배너 클릭 시 url 이동 기능 추가

--- app/src/main/java/com/mangpo/taste/view/BannerFragment.kt
+++ app/src/main/java/com/mangpo/taste/view/BannerFragment.kt
@@ -1,10 +1,28 @@
 package com.mangpo.taste.view
 
+import android.content.Intent
+import android.net.Uri
+import com.mangpo.taste.R
 import com.mangpo.taste.base.BaseFragment
 import com.mangpo.taste.databinding.FragmentBannerBinding
 
 class BannerFragment() : BaseFragment<FragmentBannerBinding>(FragmentBannerBinding::inflate) {
     override fun initAfterBinding() {
         binding.root.setImageResource(arguments?.getInt("image")!!)
+
+        binding.root.setOnClickListener {
+            when (arguments?.getInt("image")!!) {
+                R.drawable.ic_banner1 -> openLink("https://www.notion.so/5gaam/5gaam-3b45d6083ad044ab869f0df6378933de")
+                R.drawable.ic_banner2 -> {
+
+                }
+                R.drawable.ic_banner3 -> openLink("https://www.instagram.com/5gaam_app")
+            }
+        }
+    }
+
+    private fun openLink(url: String) {
+        val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+        startActivity(intent)
     }
 }
