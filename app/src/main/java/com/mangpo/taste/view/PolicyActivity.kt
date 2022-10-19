becf02d4787e0742b7e6995cc667019bac37f28f
EunhaKyeong <kyeongeh424@gmail.com>
2022-10-19 23:09:08 +0900

    Feat: 약관 텍스트 추가 완료

--- app/src/main/java/com/mangpo/taste/view/PolicyActivity.kt
+++ app/src/main/java/com/mangpo/taste/view/PolicyActivity.kt
@@ -1,16 +1,38 @@
 package com.mangpo.taste.view
 
+import com.mangpo.taste.R
 import com.mangpo.taste.base.BaseActivity
 import com.mangpo.taste.databinding.ActivityPolicyBinding
 import dagger.hilt.android.AndroidEntryPoint
+import java.io.IOException
+import java.io.InputStream
 
 @AndroidEntryPoint
 class PolicyActivity : BaseActivity<ActivityPolicyBinding>(ActivityPolicyBinding::inflate) {
     override fun initAfterBinding() {
-        binding.policyTitleTv.text = intent.getStringExtra("title")
+        val title: String = intent.getStringExtra("title")!!
+        binding.policyTitleTv.text = title
+
+        when (title) {
+            getString(R.string.title_ogam_terms) -> setText(R.raw.terms_and_conditions)
+            getString(R.string.title_privacy_policy_no_blank) -> setText(R.raw.privacy_policy)
+            getString(R.string.title_receive_marketing_information) -> setText(R.raw.receive_marketing_information)
+        }
 
         binding.policyBackIb.setOnClickListener {
             finish()
+        }
+    }
+
+    private fun setText(raw: Int) {
+        try {
+            val inputStream: InputStream = resources.openRawResource(raw)
+            val b = ByteArray(inputStream.available())
+            inputStream.read(b)
+
+            binding.policyContentTv.text = String(b)
+        } catch (e: IOException) {
+            e.printStackTrace()
         }
     }
 }
