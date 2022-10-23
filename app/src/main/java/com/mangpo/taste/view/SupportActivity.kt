799da5ed1a30cde93ff3afc48bbabdea42c41f3e
EunhaKyeong <kyeongeh424@gmail.com>
2022-10-23 22:48:57 +0900

    Design: 후원자 목록 화면 디자인 구현중

---
+++ app/src/main/java/com/mangpo/taste/view/SupportActivity.kt
@@ -0,0 +1,9 @@
+package com.mangpo.taste.view
+
+import com.mangpo.taste.base.BaseActivity
+import com.mangpo.taste.databinding.ActivitySupportBinding
+
+class SupportActivity : BaseActivity<ActivitySupportBinding>(ActivitySupportBinding::inflate) {
+    override fun initAfterBinding() {
+    }
+}
