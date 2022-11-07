090714d89023aaac4cf26b5b7571ae7bb11c5047
EunhaKyeong <kyeongeh424@gmail.com>
2022-11-07 23:48:59 +0900

    Style: 로그 코드 삭제

--- app/src/main/java/com/mangpo/taste/viewmodel/BadgeViewModel.kt
+++ app/src/main/java/com/mangpo/taste/viewmodel/BadgeViewModel.kt
@@ -1,6 +1,5 @@
 package com.mangpo.taste.viewmodel
 
-import android.util.Log
 import com.mangpo.domain.usecase.CheckThanksUseCase
 import com.mangpo.taste.base.BaseViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -11,9 +10,7 @@ class BadgeViewModel @Inject constructor(private val checkThanksUseCase: CheckTh
     fun checkThanks() {
         callApi(
             { checkThanksUseCase.invoke() },
-            {
-                Log.d("BannerViewModel", "checkThanks -> $it")
-            },
+            {},
             false
         )
     }
